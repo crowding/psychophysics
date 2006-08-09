f0944ce16ed8de1bdfa422c8138404db615f6d41
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2006-08-09 02:03:19 +0000

    transform method
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@104 2a468891-300a-0410-9f4f-8ca473c5017f

---
+++ code/old_graphics/@Calibration/transform.m
@@ -0,0 +1,28 @@
+function f = transform(this)
+    %return a function handle to a transform obejct, which transforms spatial
+    %coordinates (in pixels) to screen coordinate(in degrees) assuming the 
+    %center of the screen is at [0,0] in spaital coordinates.
+    
+    center = this.rect;
+    center = ( center([1 2]) + center([3 4]) ) / 2
+    multiplier = 1./spacing(this)
+    f = @transform;
+
+    function [x, y] = transform(x, y)
+        %support 1- and 2-argument calling styles
+        if (nargin == 2)
+            loc = [x y];
+            loc = loc .* multiplier + center;
+            x = loc(1);
+            y = loc(2);
+        else
+            %both coords are in x
+            if all(size(x) == [1 2])
+                x = x .* multiplier + center;
+            else
+                x = x .* repmat(multiplier, size(x, 1), size(x,2)/2)...
+                         + repmat(multiplier, size(x, 1), size(x, 2)/2);
+            end
+        end
+    end
+end
