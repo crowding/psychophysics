8a4d072020717ec32f2f0884280f3668e6664dcb
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2006-08-09 19:02:08 +0000

    events directory
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@106 2a468891-300a-0410-9f4f-8ca473c5017f

--- code/Trigger.m
+++ code/Trigger.m
@@ -1,13 +1,4 @@
 function this = Trigger
     %the Trigger interface.
-    this = public(...
-        @check,...
-        @id...
-    );
-    
-    function check(x, y, t)
-    end
-
-    function i = id
-    end
+    this = inherit(Identifiable(), @check);
 end
--- code/graphics/InsideTrigger.m
+++ code/graphics/InsideTrigger.m
@@ -2,7 +2,7 @@ function this = InsideTrigger(obj_, fn_)
 %An object that fires a trigger when x and y are inside the bounds of a
 %graphics object.
 
-this = inherit(Identifiable(), public(@check));
+this = inherit(Trigger(), public(@check));
     
     function check(x, y, t)
         if inRect(obj_.bounds(), x, y)
