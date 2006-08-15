3a96220b87678acc7351cd537f528363137fb2c2
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2006-08-15 17:29:16 +0000

    accessor.m should go in previous revision
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@111 2a468891-300a-0410-9f4f-8ca473c5017f

---
+++ code/accessor.m
@@ -0,0 +1,16 @@
+function [get, set] = accessor(value_)
+%creates an accessor.
+
+get = @getter;
+%might be even faster if we could avoid these function
+%handles being in the smae workspace as accessor.
+set = @setter;
+
+    function v = getter
+        v = value_;
+    end
+
+    function v = setter(v)
+        value_ = v;
+    end
+end
