60967a70fdfb76d96bd1ea1f1b1d858b1a149313
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2007-03-23 04:32:15 +0000

    some path fixes and calibration for pastorianus
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@163 2a468891-300a-0410-9f4f-8ca473c5017f

--- startup.m
+++ startup.m
@@ -4,16 +4,26 @@ function startup()
 %Place or symlink this file in the MATLAB startup directory (on OSX, this
 %is /Applications/MATLAB71 )
 
-%since matlab is dumb, it provides no provision for doing a user-specific
-%atartup script, in a way similar to every unix program in existence. wtf?
-
-%So on this machine, we will look under ~/.matlab for
-%another startup.m file.
+%We will look under ~/.matlab for another startup.m file.
 
 [status, home] = system('echo -n $HOME');
+
+toolbox = fullfile(home, '.matlab', 'toolbox');
 startupfile = fullfile(home, '.matlab', 'startup.m');
+dir = fullfile(home, '.matlab');
+
+if (exist(toolbox, 'dir') == 7)
+   addpath(toolbox);
+end
 
 if (exist(startupfile, 'file') == 2) || (exist(startupfile, 'file') == 6) 
    % 2 = 'is it a file', 6 == 'is it a P-file'
-   run(startupfile);
+   olddir = pwd();
+   cd(dir);
+   newdir = pwd();
+   startup;
+   if strcmp(newdir, pwd)
+       %this provides an "out" for startup scripts that want to set the initial path
+       cd(olddir);
+   end
 end
