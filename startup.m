5bd7b5ddd2ff392e34f32961176a58464dfc673e
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2008-03-13 08:38:15 +0000

    dp and startup
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@427 2a468891-300a-0410-9f4f-8ca473c5017f

--- startup.m
+++ startup.m
@@ -1,44 +1,7 @@
-%startup.m file
-function startup()
-
-%Place or symlink this file in the MATLAB startup directory (on OSX, this
-%is /Applications/MATLAB71 )
-
-%We will look under ~/.matlab for another startup.m file.
-
-[status, home] = system('echo -n $HOME');
-
-toolbox = fullfile(home, '.matlab', 'toolbox');
-startupfile = fullfile(home, '.matlab', 'startup.m');
-dir = fullfile(home, '.matlab');
-
-if (exist(toolbox, 'dir') == 7)
-   addpath(cleanpath(genpath(toolbox)));
-end
-
-
-%and then run the per-user config script
-if (exist(startupfile, 'file') == 2) || (exist(startupfile, 'file') == 6) 
-   % 2 = 'is it a file', 6 == 'is it a P-file'
-   olddir = pwd();
-   cd(dir);
-   newdir = pwd();
-   startup;
-   if strcmp(newdir, pwd)
-       %this provides an "out" for startup scripts that want to set the
-       %initial directory
-       cd(olddir);
-   end
-end
-
-[s, w] = system('hostname');
-if strfind(w, 'pastorianus')
-    addpath('/Users/peter/eyetracking/switchbox/shadow', '/Users/peter/eyetracking/switchbox');
-end
-
-function p = cleanpath(p)
-    %clean junk/SVN directories from a path string.
-    p = regexprep(p, '(^|:)[^:]*(\.svn|\.bundle|/private|.FBC|@)[^:]*', '');
-end
-
+%startup file for peter.
+%prepare path for eyelink project.
+run('~/eyetracking/setpath.m');
+cd('~/eyetracking/');
+if usejava('jvm')
+    AddPsychJavaPath;
 end
