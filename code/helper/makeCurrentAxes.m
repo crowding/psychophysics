2a7c3ce03a98046cf76e0eeca4e9c87dbf10f7b5
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2008-10-30 11:22:21 +0000

    make current axes function
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@489 2a468891-300a-0410-9f4f-8ca473c5017f

---
+++ code/helper/makeCurrentAxes.m
@@ -0,0 +1,13 @@
+function this = makeCurrentAxes(ax)
+
+%makes the axis and the figure containing it current WITHOUT raising it to
+%the foreground. Sheesh.
+
+fig = ancestor(x, 'figure'); %i can't make ancestor work????
+if ~isempty(fig)
+    set(0, 'CurrentFigure', fig);
+    set(fig, 'CurrentAxes', ax);
+else
+    %ffs, why won't ancestor work on my laptop?
+    axis(ax);
+end
