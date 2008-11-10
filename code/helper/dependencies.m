2bae7d4e1835270db5611610a59cf0b8fd2179f1
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2008-11-10 16:18:39 +0000

    dependencies helper
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@510 2a468891-300a-0410-9f4f-8ca473c5017f

---
+++ code/helper/dependencies.m
@@ -0,0 +1,13 @@
+function char = dependencies(mfile)
+
+%generate a space-delimited list of the user-created dependencies of an
+%M-file. This is used for makefiles.
+
+p = fdep(mfile, '-q');
+try
+    char = join(' ', p.fun);
+catch
+    char = '';
+end
+
+assignin('caller', 'response', char)
