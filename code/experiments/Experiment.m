5d6b4561e5c5b5753347ae911d23756f402a1545
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2009-01-08 21:06:27 +0000

    Experiment gui work (not tested)
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@523 2a468891-300a-0410-9f4f-8ca473c5017f

--- code/experiments/Experiment.m
+++ code/experiments/Experiment.m
@@ -17,6 +17,8 @@ filename = '__auto__';
 runs = {{}};
 description = '';
 caller = getversion(2);
+window = [];
+handles = [];
 
 params = namedargs...
         (localExperimentParams()...
@@ -70,7 +72,115 @@ end
     function setParams(varargin)
         params = namedargs(params, varargin{:});
     end
+
+    function show()
+        %build an Experiment window, or show it.
+        if isempty(window) || ~ishandle(window)
+            %set it up so that the window handle clears on closing...
+            window = figure();
+            set(window, 'DeleteFcn', @closing);
+            set(window, 'Toolbar', 'none', 'Name', caller.function, 'NumberTitle', 'off', 'MenuBar', 'none');
+            
+            %create in it some panels.
+            handles.prop_panel = uipanel('Parent', window, 'Position', [0 0 0.5 0.9], 'Title', 'Properties');
+            handles.button_panel = uipanel('Parent', window, 'Position', [0 0.9 0.5 0.1], 'Title', 'Run/Stop');
+            handles.trial_panel = uipanel('Parent', window, 'Position', [0.5 0.5 0.5 0.5], 'Title', 'Last trial');
+            handles.trial_axes = axes('Parent', handles.trial_panel);
+            handles.experiment_panel = uipanel('Parent', window, 'Position', [0.5 0 0.5 0.5], 'Title', 'Experiment');
+            handles.experiment_axes = axes('Parent', handles.experiment_panel); 
+            
+            %create the start and stop buttons.
+            handles.start_button = uicontrol(handles.button_panel, 'Style', 'pushbutton', 'Units', 'Normalized', 'String', 'Start', 'Position', [0 0 0.5 1], 'BackgroundColor', [0.5 1 0] );
+            handles.stop_button = uicontrol(handles.button_panel, 'Style', 'pushbutton', 'Units', 'Normalized', 'String', 'Stopped', 'Position', [0.5 0 0.5 1], 'BackgroundColor', [0.8 0.8 0.8], 'Enable', 'off');
+            
+            set(handles.start_button, 'Callback', @start_);
+            set(handles.stop_button, 'Callback', @stop_);
+            
+            %create the property explorer.
+        else
+            figure(window);
+        end
         
+        function closing(hObject, edata, h) %#ok
+            window = [];
+            handles = [];
+        end
+    end
+
+    function start_(hObject, edata, h)
+        require(@showStarting_, this.run);
+    end
+
+    function stop_(hObject, edata, h)
+        error('a:a', 'Don''t know how to stop!');
+    end
+
+    function [release, params] = showRunning_(params)
+        if ~isempty(handles)
+            prevstart = getandset_(handles.start_button, 'BackgroundColor', [0.8 0.8 0.8], 'Enable', 'off', 'String', 'Running');
+            prevstop = getandset_(handles.stop_button, 'BackgroundColor', [1 0.1 0.1], 'Enable', 'on', 'String', 'Stop');
+        end
+        
+        release = @r;
+        function r()
+            if ~isempty(handles)
+                set(handles.start_button, prevstart{:});
+                set(handles.start_button, prevstop{:});
+            end
+        end
+    end
+
+    function [release, params] = showStopping_(params)
+        if ~isempty(handles)
+            prev = getandset(handles.stop_button, 'BackgroundColor', [1 0.1 0.1], 'Enable', 'off', 'String', 'Stopping');
+        end
+        
+        release = @r;
+        function r()
+            if ~isempty(handles)
+                set(handles.stop_button, prev{:});            
+            end
+        end
+    end
+
+    function [release, params] = showSaving_(params)
+        if ~isempty(handles)
+            prev = getandset_(handles.start_button, 'BackgroundColor', [0.8 0.8 0.8], 'Enable', 'off', 'String', 'Saving...');
+            prev = getandset_(handles.stop_button, 'BackgroundColor', [0.8 0.8 0.8], 'Enable', 'off', 'String', 'Saving...');
+        end
+        
+        release = @r;
+        function r()
+            if ~isempty(handles)
+                set(handles.stop_button, prev{:});            
+            end
+        end
+    end
+
+
+    function [release, params] = showStarting_(params)
+        if ~isempty(handles)
+            prev = getandset_(handles.start_button, 'Enable', 'off', 'String', 'Starting...');
+        end
+        
+        release = @r;
+        function r()
+            if ~isempty(handles)
+                set(handles.stop_button, prev{:});            
+            end
+        end
+    end
+
+    function prev = getandset_(obj, varargin)
+        %set a list of properties, but return the previous values (as a cell list of arguments) first.
+        args = reshape(varargin, 2, []);
+        for i = 1:size(args, 2)
+            args{i,2} = get(obj, args{i,1});
+        end
+        prev = reshape(args, 1, []);
+        set(obj, varargin{:});
+    end
+
     function run(varargin)
         if isempty(subject)
             subject = input('Enter subject initials: ', 's');
@@ -81,6 +191,9 @@ end
         end
 
         params = namedargs(params, varargin{:});
+        if ~isempty(handles)
+            params.uihandles = handles;
+        end
 
         if isfield(trials, 'startBlock')
             this.trials.startBlock(); %IT IS CALLED TWICE IF CONTINUED?
@@ -112,13 +225,13 @@ end
             , 'caller', caller...
             );
         e = [];
-        try
+        try %GAH. this should be abstracted and put in some per host config.
             [stat, host] = system('hostname');
             if ~isempty(strfind(host, 'pastorianus'))
                 switchscreen('videoIn', 1, 'videoOut', 1, 'immediate', 1);
-                theRun.run();
+                require(@showRunning, theRun.run);
             else
-                theRun.run();
+                require(@showRunning, theRun.run);
             end
         catch
             theRun.setErr(lasterror);
@@ -127,24 +240,28 @@ end
             end
         end
         
-        this.runs{end+1} = theRun;
+        require(@showSaving_, @save);
+        function save(a)
+            this.runs{end+1} = theRun;
 
-        %now save ourself. Since we overwrite, it is prudent to write
-        %to a temp file first.
-        if(~isempty(filename))
-            t = tempname;
-            disp( sprintf('writing to temp file %s', t));
-            require(openFile(t, 'w'), @(params) dump(this, @(pat, varargin)fprintf(params.fid, [pat '\n'], varargin{:}), 'experiment'));
-            finalfile = fullfile(env('datadir'), [this.filename '.txt']);
-            movefile(t, finalfile);
-            disp( sprintf('saved to %s', finalfile) );
-        end
+            %now save ourself. Since we overwrite, it is prudent to write
+            %to a temp file first.
 
-        %if there was an error, report it after saving
-        if ~isempty(theRun.getErr())
-            err = theRun.getErr();
-            warning('the run stopped with an error: %s', err.identifier)
-            stacktrace(err);
+            if(~isempty(filename))
+                t = tempname;
+                disp( sprintf('writing to temp file %s', t));
+                require(openFile(t, 'w'), @(params) dump(this, @(pat, varargin)fprintf(params.fid, [pat '\n'], varargin{:}), 'experiment'));
+                finalfile = fullfile(env('datadir'), [this.filename '.txt']);
+                movefile(t, finalfile);
+                disp( sprintf('saved to %s', finalfile) );
+            end
+
+            %if there was an error, report it after saving.
+            if ~isempty(theRun.getErr())
+                err = theRun.getErr();
+                warning('the run stopped with an error: %s', err.identifier)
+                stacktrace(err);
+            end
         end
     end
 
