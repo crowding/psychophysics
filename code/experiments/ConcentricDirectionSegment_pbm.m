f92d7b6186e8173b9159cf43e6306f76abf0d3f7
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2011-09-07 21:57:20 +0000

    code updates
    git-svn-id: file:///home/peterm/svn/eyetracking/trunk@717 2a468891-300a-0410-9f4f-8ca473c5017f

--- code/experiments/ConcentricDirectionSegment_pbm.m
+++ code/experiments/ConcentricDirectionSegment_pbm.m
@@ -1,5 +1,6 @@
 function exp = ConcentricDirectionSegment_pbm(exp)
     disp('configuring for pbm');
     exp.trials.replace('extra.side', {'left', 'right', 'right', 'left', 'left', 'right', 'right', 'left'}, 1, 1); %side is blocked
+%    exp.trials.replace('extra.side', {'top', 'bottom', 'bottom', 'top', 'top', 'bottom', 'bottom', 'top'}, 1, 1); %side is blocked
     exp.trials.blockSize = exp.trials.numLeft() / 4;
 end
