8dc1584387ba25da758bebaf11a254c34e7270e1
Peter Meilstrup <peter.meilstrup@gmail.com>
2012-12-13 00:30:36 -0800

    get git revision info in getversion

--- code/infrastructure/getversion.m
+++ code/infrastructure/getversion.m
@@ -95,13 +95,14 @@ else
 end
 
 
-vcsinfo = findvcs(frame.file);
-v = vcsinfo(frame.file);
+[vcsinfo, vcsdir, vcspath] = findvcs(frame.file);
+v = vcsinfo(frame.file, vcsdir, vcspath);
 cache(1).(fieldname) = v;
 
-function vcsfun = findvcs(file)
+function [vcsfun, dir, relative] = findvcs(file)
     %which VCS are we running? Let's scan up for the hidden directories.
-    dir = fileparts(file);
+    [dir, relative, ext] = fileparts(file);
+    relative = [relative ext];
 
     while(1)
         if exist(fullfile(dir, '.git'), 'dir')
@@ -124,33 +125,52 @@ function vcsfun = findvcs(file)
             end
             return
         end
-        parent = fileparts(dir);
+        [parent, next] = fileparts(dir);
         if strcmp(parent, dir)
-            break
+            break;
         end
+        relative = fullfile(next, relative);
         dir = parent;
     end
 
     vcsfun = @nullinfo;
+    [dir, relative] = fileparts(file);
 end
 
-function v = nullinfo(file)
+function [v, d] = nullinfo(file, dir, relative)
     warning('getversion:noRepositoryFound', ...
             ['The file "%s" does not appear to be in version control, '...
              'or the version control program was not found.'], file);
     v = struct('function', frame.name, 'file', file);
+    d = '';
 end
 
-function c = gitinfo(file)
-    [status, info] = shellcommand(gitexec, 'status', '--', file);
-    error('getverion:unimplemented', 'obtaining git info not implemented yet');
+function info = gitinfo(file, dir, relative)
+    [status, gitstatus] = ...
+        shellcommand(gitexec, ['--git-dir=', fullfile(dir, '.git')], ...
+                     'status', '-u', '-z', '--porcelain', '--', relative);
+    [status, repo_rev] = ...
+        shellcommand(gitexec, ['--git-dir=', fullfile(dir, '.git')], ...
+                     'rev-list', '--max-count=1', '--header', ...
+                     '--pretty=format:', 'HEAD');
+    [status, file_last_rev] = ...
+        shellcommand(gitexec, ['--git-dir=', fullfile(dir, '.git')], ...
+                     'rev-list', '--max-count=1', '--pretty=format:', ...
+                     'HEAD', '--', relative);
+
+    gitstatus = gitstatus(1:min(2, numel(gitstatus)));
+    repo_rev = repo_rev(min(8, numel(repo_rev)):max(end-1,0));
+    file_last_rev = file_last_rev(min(8, numel(file_last_rev)):max(end-1,0));
+
+    info = struct('function', frame.name, 'git_repo', dir, 'path', relative, ...
+                  'head_rev', repo_rev, 'last_modified_rev', file_last_rev, ...
+                  'status', gitstatus, 'parents', {{}});
 end
 
-function v = svninfo(file)
+function v = svninfo(file, dir)
     %grab the version information from SVN
 
-    [status, info] = shellcommand(svnexec, 'info', '--', file, frame.file);
-
+    [s, status] = shellcommand(svnexec, 'info', '--', file, frame.file);
 
     if status ~= 0
         warning('getversion:svn', 'couldn''t call svn on %s', frame.file);
